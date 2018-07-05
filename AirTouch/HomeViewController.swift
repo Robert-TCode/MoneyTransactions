75e1c4bec550c3029a2879a0c5f759cfc28522e3
Robert Tanase <robert.tanase@cst-dev.com>
2018-07-05 09:24:49 +0300

    Initial commit

---
+++ AirTouch/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  AirTouch
+//
+//  Created by Robert Tanase on 05/07/2018.
+//  Copyright © 2018 AirTouch. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
